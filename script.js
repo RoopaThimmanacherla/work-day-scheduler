c1d134752fbab1428d733dea0c7e719b0e13152d
Roopa Thimmanacherla <roopareddy90@gmail.com>
2023-11-04 10:34:51 -0700

    updated js file to save data to the local storage

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+var currentDay = $("#currentDay");
+currentDay.text(dayjs().format("dddd,MMMM DD"));
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -8,6 +11,14 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
+  $(".saveBtn").on("click", function () {
+    var time = $(this).parent().attr("id");
+    console.log(time);
+    var text = $(this).siblings(".description").val();
+    console.log(text);
+    localStorage.setItem(time, text);
+  });
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
